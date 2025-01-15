0a0d2f154bb0d10df232c2cba2ff8e4f9b5e7e9c
Benjamín Quezada <b.quezadagodoy@gmail.com>
2025-01-15 15:04:12 -0300

    commit2

---
+++ 01-basic/02_types.py
@@ -0,0 +1,25 @@
+print("int:")
+# print(type(10))
+
+print("float:")
+# print(type(10.5))
+# print(type(10.0))
+
+
+print ("complex:")
+# print(type(10+5j))
+
+
+print("str:")
+# print(type("Hola"))
+# print(type(''))
+
+
+print("bool:")
+# print(type(True))
+# print(type(False))
+# print(type(9 < 10))
+
+
+print("Nonetype:")
+print(type(None))
