f2149f74bc4141b7c3e2904769ccf469dc166b41
Benjamín Quezada <b.quezadagodoy@gmail.com>
2025-01-15 15:11:53 -0300

    commit 3

---
+++ 01-basic/03_cast.py
@@ -0,0 +1,24 @@
+# 03 - casting de tipos
+# Transformar un tipo de un valor a otro
+
+print("Conversion de tipos")
+# print(type("100"))
+# print(type(int("100")))
+
+
+# print(int("100") + 2)
+
+# print(type(float("100.5")))
+# print(int(100.5))
+
+# print(bool(3))
+# print(bool(0))
+# print(bool(-1))
+
+
+# print(bool(""))
+# print(bool(" "))
+# print(bool("False"))
+
+
+# print(int("Hola mundo"))
